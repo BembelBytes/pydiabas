a5a405943bb2c5fcc024cfb0f041bf7c3d5bceb1
Aljoscha Greim <aljoscha@bembelbytes.com>
2024-12-30 00:02:59 +0100

    typo

--- src/test/__init__.py
+++ src/test/__init__.py
@@ -8,10 +8,10 @@ from .test_ecu import *
 
 """Using these tests:
 
-All of the tetst concerning pydiabas, ediabas and the test_base test in test_ecu can be run
+All of the test concerning pydiabas, ediabas and the test_base test in test_ecu can be run
 without being connected to a car or even having an USB CAN cable connected.
 Only these tests will be executed when running the test module.
-There are furhter test concerning specific ECUs. These test need a active connection to a ECU of this kind.
+There are further test concerning specific ECUs. These test need a active connection to a ECU of this kind.
 To run test for the MSD80 class, you need to be connected to a car using an MSD80 ECU.
 These test need to be run manually!
 
